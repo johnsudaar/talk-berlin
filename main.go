129e5e0b620e1755ccd5a25b82836835cf2f2a32
Jonathan Hurter <john@scalingo.com>
2017-07-04 14:09:36 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := "3000"
+	if len(os.Getenv("PORT")) != 0 {
+		port = os.Getenv("PORT")
+	}
+
+	http.HandleFunc("/cli", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello PaaS meetup !")
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		f, _ := os.Open("./templates/index.html")
+		io.Copy(w, f)
+		f.Close()
+	})
+
+	log.Println("Listening on: " + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+}
